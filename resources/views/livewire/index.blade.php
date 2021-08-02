a8b6b5d00875caa9a44428a74bcc2ee481e678fc
IscoNava <60551759+IscoNava@users.noreply.github.com>
2021-08-02 15:35:10 -0500

    Prueba Versionamiento

--- resources/views/livewire/index.blade.php
+++ resources/views/livewire/index.blade.php
@@ -11,7 +11,7 @@
 <div class="w-full bg-center h-screen bg-gray-900 pattern">
     <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
         <div class="text-center">
-            <h1 class="text-2xl font-semibold text-white uppercase md:text-3xl">Bienvenid@ <span class="text-blue-400 underline">Hola!</span></h1>
+            <h1 class="text-2xl font-semibold text-white uppercase md:text-3xl">Bienvenid@ <span class="text-blue-400 underline">Hola! Versionamiento 02/08/2021</span></h1>
             <!--<button class="px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500"><a href="#">Iniciar Sesión</a></button>-->
         </div>
     </div>
